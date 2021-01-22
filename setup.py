24b8f2d965d27ff7d454c616a11fc0ca764236f8
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-21 19:02:37 -0800

    Updated package dist.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webcontrollers',
-    version='0.0.10',
+    version='0.0.11',
     description='A package to faciliate the interaction with CAASPP, SEIS, and more.',
     url='https://github.com/GitPushPullLegs/webcontrollers',
     author='Joe Aguilar',
--- webcontrollers/caaspp/__init__.py
+++ webcontrollers/caaspp/__init__.py
@@ -1,5 +1,5 @@
 from .tomscontroller import TOMSController
 from .cerscontroller import CERSController
 
-__version__ = '0.0.10'
+__version__ = '0.0.11'
 __author__ = 'Joe Aguilar'
