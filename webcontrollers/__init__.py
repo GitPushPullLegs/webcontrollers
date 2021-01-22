afa8b65dcdf553224e96a176ad310869edd936ce
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-21 18:19:37 -0800

    Corrected missing common.

--- webcontrollers/__init__.py
+++ webcontrollers/__init__.py
@@ -3,5 +3,5 @@ from .caaspp.tomscontroller import TOMSController
 from .caaspp.cerscontroller import CERSController
 
 
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __author__ = 'Joe Aguilar'
--- webcontrollers/common/__init__.py
+++ webcontrollers/common/__init__.py
@@ -1,1 +1,4 @@
 from .webcontroller import WebController
+
+__version__ = '0.0.4'
+__author__ = 'Joe Aguilar'
