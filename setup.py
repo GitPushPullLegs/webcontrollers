a3fc19537aacd5314ded7dbd6bfdd57f93759c92
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-21 17:59:10 -0800

    Corrected missing common package.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='webcontrollers',
@@ -8,7 +8,7 @@ setup(
     author='Joe Aguilar',
     author_email='jose.aguilar.6694@gmail.com',
     license='MIT License',
-    packages=['webcontrollers'],
+    packages=find_packages(),
     install_requires=['selenium'],
     classifiers=[
         'Development Status :: 1 - Planning',
