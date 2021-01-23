95345aa70ab2f787e18de5d8999f9e10257df019
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-22 17:36:35 -0800

    Developed login method for SEISController.

--- webcontrollers/seis/seiscontroller.py
+++ webcontrollers/seis/seiscontroller.py
@@ -1,9 +1,21 @@
 from webcontrollers.common import WebController
 from selenium.common.exceptions import *
 from webcontrollers.common.errors import *
+from wait import BrowserWait
 
 
 class SEISController(WebController):
 
     def login(self, username: str, password: str):
-        print("username")
+        self.driver.get('https://seis.org/login')
+        BrowserWait(browser=self.driver).until_angular_loads()
+
+        self.driver.find_element_by_id('username').send_keys(username)
+        self.driver.find_element_by_id('password').send_keys(password)
+        self.driver.find_element_by_xpath('//*[text()="Login"]')
+
+        try:
+            if self.driver.find_element_by_xpath('//*[text()="Forgot Password Link"]'):
+                raise AuthenticationError("Username or password incorrect.")
+        except Exception as exc:
+            pass
