ac98f236896992c363acb3b83670c76792e5c69a
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-01-25 19:33:57 -0800

    Created login function and convenience function to read school ID numbers.

--- webcontrollers/schoology/schoologycontroller.py
+++ webcontrollers/schoology/schoologycontroller.py
@@ -9,5 +9,49 @@ from webcontrollers.common.errors import *
 
 class SchoologyController(WebController):
 
-    def login(self, username:str, password: str):
-        print("Hello moto.")
+    def login(self, username:str, password: str, school: str, school_id: str):
+        """
+        Logs into Schoology.
+        :param username:
+        :param password:
+        :param school:
+        :param school_id: The school id number.
+        :return:
+        """
+        self.driver.get('https://app.schoology.com/login')
+        self.driver.find_element_by_id('edit-mail').send_keys(username)
+        self.driver.find_element_by_id('edit-pass').send_keys(password)
+        self.driver.find_element_by_id('edit-school').send_keys(school)
+
+        self.driver.execute_script("""document.getElementsByName('school_nid')[0].type = 'text';""")
+        self.driver.find_element_by_id('edit-school-nid').send_keys(school_id)
+
+        self.driver.find_element_by_id('edit-submit').click()
+
+    def export_nids_to_csv(self, school: str, outfile_path: str):
+        """
+        Convenience function to export the school information.
+        Returns more than one result so you can enter partial names.
+        :param school: The partial or full name of the school whose ID you're looking for.
+        :param outfile_path: The file to be created, must end in .csv.
+        """
+        if not outfile_path.endswith(".csv"):
+            raise AttributeError("outfile_path must end with .csv.")
+
+        import csv  # This function is for convenience, it will not be used often and we don't need to import csv for the rest of the module.
+        import json
+        import re
+
+        school = school.replace(" ", "%20")
+        self.driver.get(f'https://app.schoology.com/register/ajax/school?jq={school}&limit=40')
+
+        data = json.loads(re.findall(r'(?<=white-space: pre-wrap;\">)[\[{\"A-Za-z0-9:, _.\\\/?=}\-()\]]+(?=</pre)', self.driver.page_source)[0])
+
+        with open(outfile_path, 'w', newline='') as file:
+            csv_writer = csv.writer(file)
+            header = True
+            for school in data:
+                if header:
+                    csv_writer.writerow(school.keys())
+                    header = False
+                csv_writer.writerow(school.values())
