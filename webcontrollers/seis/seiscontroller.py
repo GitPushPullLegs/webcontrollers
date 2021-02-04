ac337137c752bfd998d968b0ab845a100002afae
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-03 19:16:44 -0800

    Corrected browser to driver.

--- webcontrollers/seis/seiscontroller.py
+++ webcontrollers/seis/seiscontroller.py
@@ -17,12 +17,12 @@ class SEISController(WebController):
         :param password:
         """
         self.driver.get('https://seis.org/login')
-        BrowserWait(browser=self.driver).until_angular_loads()
+        BrowserWait(driver=self.driver).until_angular_loads()
 
         self.driver.find_element_by_id('username').send_keys(username)
         self.driver.find_element_by_id('password').send_keys(password)
         self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[2]/div/button').click()
-        BrowserWait(browser=self.driver).until_angular_loads()
+        BrowserWait(driver=self.driver).until_angular_loads()
 
         try:
             if self.driver.find_element_by_xpath('//*[@id="loginForm"]/div[3]/div'):
@@ -37,9 +37,9 @@ class SEISController(WebController):
         Wait().until(self._report_ready, requested_time=requested_time)
 
         self.driver.find_elements_by_xpath('//*[text()="XLSX"]')[1].click()
-        BrowserWait(browser=self.driver).until_angular_loads()
+        BrowserWait(driver=self.driver).until_angular_loads()
 
-        return BrowserWait(browser=self.driver).until_file_downloaded()
+        return BrowserWait(driver=self.driver).until_file_downloaded()
 
     def _report_ready(self, requested_time):
         """Checks if the latest report came after the request."""
@@ -51,11 +51,11 @@ class SEISController(WebController):
     def download_toms_summative_report(self):
         """Downloads the TOMS Summative Test Settings Report and returns the file path."""
         self.driver.get('https://seis.org/reports/toms')
-        BrowserWait(browser=self.driver).until_angular_loads()
+        BrowserWait(driver=self.driver).until_angular_loads()
         return self._download_report()
 
     def download_toms_interim_report(self):
         """Downloads the TOMS Interim Test Settings Report and returns the file path."""
         self.driver.get('https://seis.org/reports/tomsinterim')
-        BrowserWait(browser=self.driver).until_angular_loads()
+        BrowserWait(driver=self.driver).until_angular_loads()
         return self._download_report()
