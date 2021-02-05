a7789b8d3497df395ee624d8b90aad06fe60f048
GitPushPullLegs <jose.aguilar.6694@gmail.com>
2021-02-04 16:39:34 -0800

    Updated distribution

--- webcontrollers/__init__.py
+++ webcontrollers/__init__.py
@@ -5,5 +5,5 @@ from .seis import SEISController
 from .schoology import SchoologyController
 from .calpads import CALPADSController
 
-__version__ = '0.0.15'
+__version__ = '0.0.16'
 __author__ = 'Joe Aguilar'
